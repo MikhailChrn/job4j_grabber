7e7563338f5cba318520bf6222d6e513dc7249ff
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-10-02 19:08:59 +0400

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -7,13 +7,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-
-/**
- * 1. Доработайте программу из задания таким образом,
- * что она еще выводила дату вакансии,
- * т.е. вам нужно извлечь значение атрибута элемента на рисунке ниже
- */
 
 public class HabrCareerParse {
 
@@ -22,17 +15,19 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            Element dateElement = row.select(".vacancy-card__date").first();
-            String lt = dateElement.child(0).attr("datetime");
-            System.out.printf("%s %s %s%n", vacancyName, lt, link);
-        });
+        for (int i = 1; i <= 5; i++) {
+            Connection connection = Jsoup.connect(String.format("%s%s%d", PAGE_LINK, "?page=", i));
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                Element dateElement = row.select(".vacancy-card__date").first();
+                String lt = dateElement.child(0).attr("datetime");
+                System.out.printf("%s %s %s%n", vacancyName, lt, link);
+            });
+        }
     }
 }
