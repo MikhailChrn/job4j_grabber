dbc41ed29e334fb57067cf30b96afd20d6dde9ad
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-10-11 07:40:12 +0400

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -1,4 +1,4 @@
-package ru.job4j.model;
+package ru.job4j.grabber;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -21,8 +21,36 @@ public class Post {
         this.created = created;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     @Override
